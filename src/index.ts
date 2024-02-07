170988255ecbbe63ab3bc31b97e29919c70446f8
Mallory Hayr <hey@igalaxy.dev>
2024-02-07 15:49:15 -0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,137 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Feed } from 'feed';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+export interface Env {}
+
+enum VersionType {
+	SNAPSHOT = 'snapshot',
+	RELEASE = 'release',
+}
+
+interface RawPatchnotes {
+	version: number;
+	entries: RawPatchnotesEntry[];
+}
+
+interface RawPatchnotesEntry {
+	title: string;
+	version: string;
+	type: VersionType;
+	image: {
+		title: string;
+		url: string;
+	};
+	contentPath: string;
+	id: string;
+}
+
+interface PatchnotesEntry {
+	title: string;
+	time: string;
+	version: string;
+	type: VersionType;
+	image: {
+		title: string;
+		url: string;
+	};
+	body: string;
+	id: string;
+}
+
+interface PistonMeta {
+	latest: {
+		release: string;
+		snapshot: string;
+	};
+	versions: PistonMetaVersion[];
+}
+
+interface PistonMetaVersion {
+	id: string;
+	type: string;
+	url: string;
+	time: string;
+	releaseTime: string;
+	sha1: string;
+	complianceLevel: number;
+}
+
+interface LauncherContentEntry {
+	title: string;
+	version: string;
+	type: string;
+	image: {
+		title: string;
+		url: string;
+	};
+	body: string;
+	id: string;
+}
+
+const META_BASE_URL = 'https://piston-meta.mojang.com';
+const LAUNCHER_BASE_URL = 'https://launchercontent.mojang.com';
+
+let pistonMeta: PistonMeta;
+
+async function fetchPistonMeta() {
+	if (pistonMeta == null) pistonMeta = await (await fetch(META_BASE_URL + '/mc/game/version_manifest_v2.json')).json();
+}
+
+async function getPatchnotes(raw: RawPatchnotesEntry): Promise<PatchnotesEntry> {
+	await fetchPistonMeta();
+	const meta: PistonMetaVersion = pistonMeta.versions.find((x) => x.id == raw.version) || {
+		id: '',
+		type: '',
+		url: '',
+		time: '',
+		releaseTime: '',
+		sha1: '',
+		complianceLevel: -1,
+	};
+	const content: LauncherContentEntry = await (await fetch(LAUNCHER_BASE_URL + '/v2/' + raw.contentPath)).json();
+
+	return {
+		title: content.title,
+		time: meta.time,
+		version: meta.id,
+		type: raw.type,
+		image: {
+			title: content.image.title,
+			url: LAUNCHER_BASE_URL + content.image.url,
+		},
+		body: content.body,
+		id: raw.id,
+	};
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const feed = new Feed({
+			title: 'Minecraft Patch Notes',
+			description: 'Patch notes for Minecraft: Java Edition',
+			id: 'https://' + new URL(request.url).host + '/',
+			link: 'https://quiltmc.org/en/mc-patchnotes/',
+			language: 'en',
+			favicon: 'https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/favicon.ico',
+			copyright: `All rights reserved 2009-${new Date().getFullYear()}, Mojang`,
+		});
+
+		let rawPatchnotes: RawPatchnotes = await (await fetch(LAUNCHER_BASE_URL + '/v2/javaPatchNotes.json')).json();
+		for (const entry of rawPatchnotes.entries.slice(0, 10)) {
+			const patchnotes = await getPatchnotes(entry);
+			const url = `https://quiltmc.org/en/mc-patchnotes/#${patchnotes.version}`;
+			feed.addItem({
+				title: patchnotes.title,
+				id: url,
+				link: url,
+				content: patchnotes.body,
+				date: new Date(patchnotes.time),
+				image: patchnotes.image,
+			});
+		}
+		return new Response(feed.rss2(), {
+			headers: {
+				'content-type': 'application/rss+xml',
+			},
+		});
 	},
 };
